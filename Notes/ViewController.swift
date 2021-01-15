e12194f6a11a4a6c6a7b362e5df8bbf4a7bcd5a8
piladzisi <piladzisi@gmail.com>
2021-01-16 00:21:52 +0200

    update note

--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import CoreData
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     
     var notes = [Note]()
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
         navigationController?.navigationBar.barTintColor  = .systemOrange // for small titles
         navigationController?.view.backgroundColor = .systemOrange // for large titles
         tableView.dataSource = self
+        tableView.delegate = self
         loadNotes()
     }
     
@@ -45,7 +46,7 @@ class ViewController: UIViewController {
             guard
                 let noteBody = alert.textFields?.first?.text,
                 let appDelegate = UIApplication.shared.delegate as? AppDelegate
-                else { return }
+            else { return }
             
             let context = appDelegate.persistentContainer.viewContext
             let newNote = Note(context: context)
@@ -60,16 +61,45 @@ class ViewController: UIViewController {
     }
 }
 
-extension ViewController: UITableViewDataSource {
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         notes.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = notes[indexPath.row].body
+        cell.selectionStyle = .none
+        let note = notes[indexPath.row]
+        cell.textLabel?.text = note.body
         cell.textLabel?.numberOfLines = 0
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let note = notes[indexPath.row]
+        
+        let alert = UIAlertController(title: "Edit Note", message: nil, preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.text = note.body
+        }
+        
+        let updateAction = UIAlertAction(title: "Save", style: .default) { (_) in
+            guard
+                let updatedNoteBody = alert.textFields?.first?.text,
+                let appDelegate = UIApplication.shared.delegate as? AppDelegate
+            else { return }
+          
+            note.body = updatedNoteBody
+            appDelegate.saveContext()
+            
+            self.loadNotes()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alert.addAction(cancelAction)
+        alert.addAction(updateAction)
+        present(alert, animated: true)
     }
 }
 
